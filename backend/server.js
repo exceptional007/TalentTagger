5833fb9380ad0d6f2a3a717e54ecc4a66c0f4d1a
Akshhat Srivastava <akshhatsri7843@gmail.com>
2025-05-20 16:23:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://earnest-meerkat-e5b519.netlify.app/",
   credentials: true
 }));
 
